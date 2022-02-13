9b8f7096579cf8536ebb312ffdc2442a725e277f
SheikhRashed <devrashed06@gmail.com>
2022-02-13 15:58:09 +0600

    update: colors plate inserted

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
 
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'gray-71': ' #5B6771',
+				'white-fb': '#FBFBFB',
+				'white-cf': '#F4E5CF',
+				'green-40': '#8DC340',
+				'orange-26': '#FAA426',
+				'blue-aa': '#0F5EAA',
+				'pink-81': '#A72381',
+				'red-29': '#CA2729',
+				'orange-2a': '#CF692A',
+			},
+		},
 	},
 	variants: {
 		extend: {},
