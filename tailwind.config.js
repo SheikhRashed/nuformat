5bd8b97c7f82bb80989bc4d74667d112691b80a0
SheikhRashed <devrashed06@gmail.com>
2022-02-13 15:46:59 +0600

    update: primary setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ['./src/sass/**/*.{sass,scss,css}', './**/*.html'],
+
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
